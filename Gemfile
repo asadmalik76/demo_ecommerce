9392903ae118e125c9ed31a0f0796fbd63b010fa
asadmalik76 <malik.asad.7334@gmail.com>
2022-08-02 19:58:36 +0500

    Intergrate devise with seller and buyer role

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ ruby "2.7.0"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 
+gem 'devise'
+gem "rolify"
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users, controllers: {
+    :sessions => "users/sessions",
+    :registrations => "users/registrations" }
+
+
+  devise_scope :user do
+  # resources :sellers
+  resources :sellers
+  # get "/sellers_registeration", to: "sellers#new"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Defines the root path route ("/devise_scope")
+  root "index#index"
 end
